0ad9f5f6c97f505c93b286e73500fab263a24387
crisstanza <crisstanza@work>
2012-09-20 01:05:36 -0300

    Some click control, new screen to show messages

--- js/js.js
+++ js/js.js
@@ -1,8 +1,45 @@
 (function () {
 	//
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//
 	function $(id) {
 		return document.getElementById(id);
 	}
+	function show(element) {
+		return element.style.display = '';
+	}
+	function hide(element) {
+		return element.style.display = 'none';
+	}
+	//
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//
+	function MyGame() {
+	}
+	//
+	MyGame.prototype = {
+		currentPlayer: 'O',
+		nextTurn: function() {
+			switch(this.currentPlayer) {
+				case 'O':
+					this.currentPlayer = 'X';
+					break;
+				case 'X':
+					this.currentPlayer = 'O';
+					break;
+			}					
+		},
+		checkGameOver: function() {
+
+			// todo: check if is really game over!!!
+
+			return false;
+		}
+	};
+	//
+	var myGame = new MyGame();
+	//
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//
 	function userClick(evt) {
 		//
@@ -13,13 +50,20 @@
 		//
 		switch(currentClass) {
 			case '':
-				cell.setAttribute('class', 'O');
+				cell.setAttribute('class', myGame.currentPlayer);
+				//
+				var isGameOver = myGame.checkGameOver();
+				if ( isGameOver ) {
+					gameOver();
+				} else {
+					myGame.nextTurn();
+				}
 				break;
 			case 'O':
-				cell.setAttribute('class', 'X');
+				// cell.setAttribute('class', 'X');
 				break;
 			case 'X':
-				cell.setAttribute('class', '');
+				// cell.setAttribute('class', '');
 				break;
 		} 
 	}
@@ -36,9 +80,25 @@
 			for ( var i = 0 ; i < hor ; i++ ) {
 				var cell = board.rows[i].cells[j];
 				cell.addEventListener('click', userClick, false);
-
+				//
+				cell.setAttribute('data-x', i);
+				cell.setAttribute('data-y', j);
 			}
 		}
+		//
+		var mainMessage = $('main_message');
+		hide(mainMessage);
+	}
+	//
+	function checkGameOver() {
+		//
+		console.log('checkGameOver()');
+		//
+		var isGameOver = myGame.checkGameOver();
+		if ( isGameOver ) {
+			// todo: update UI
+		}
+		return isGameOver;
 	}
 	//
 	window.onload = init;
